cba3eb87cf61b131baf8897bb2308c67017db42c
Ozbbi <ozuysalcaboramir@gmail.com>
2026-06-13 20:28:35 +0300

    fix: remove Clerk dependency from middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,1 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-    '/mentor(.*)',
-      '/analytics(.*)',
-      ]);
-
-      export default clerkMiddleware((auth, req) => {
-        if (isProtectedRoute(req)) {
-            auth().protect();
-              }
-              });
-
-              export const config = {
-                matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-                };
+export {};
